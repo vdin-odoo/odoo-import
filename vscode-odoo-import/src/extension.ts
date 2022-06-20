400062cdf1e7b319abe5d6de004ad2b54ad3ea9c
Viet Dinh <54ckb0y789@gmail.com>
2022-06-19 21:53:32 -0400

    Scaffold VSCode extension

---
+++ vscode-odoo-import/src/extension.ts
@@ -0,0 +1,20 @@
+import * as vscode from "vscode";
+
+export async function activate(context: vscode.ExtensionContext) {
+  const tsExtension = vscode.extensions.getExtension('vscode.typescript-language-features')
+  if (!tsExtension) return
+  await tsExtension?.activate()
+
+  if (!tsExtension.exports?.getApi) return
+  const api = tsExtension.exports.getAPI(0)
+  if (!api) return
+
+  api.configurePlugin('odoo-import', {})
+
+  console.log(
+    'Congratulations, your extension "vscode-odoo-import" is now active!'
+  );
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}

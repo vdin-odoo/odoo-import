990e7e195b88c957f6a1a63be22dabb5c7867415
Viet Dinh <vidi@odoo.com>
2022-07-11 18:07:00 -0400

    Configure onConfigurationUpdated

--- vscode-odoo-import/src/extension.ts
+++ vscode-odoo-import/src/extension.ts
@@ -17,4 +17,4 @@ export async function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
